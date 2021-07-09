9347baa3b5fed29116e59d70a9e68618f25daabc
praveen <praveenbhananjaya@gmail.com>
2021-07-10 01:07:13 +0530

    step

---
+++ software/src/main/java/UI/Table/Table.java
@@ -0,0 +1,11 @@
+package UI.Table;
+
+public class Table {
+    public static void main(String[] args) {
+
+    }
+    public int test(){
+        System.out.println("ok");
+        return 2 ;
+    }
+}
---
+++ software/src/test/java/UI/Table/TableTest.java
@@ -0,0 +1,27 @@
+package UI.Table;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Test
+    void test1() {
+        assertEquals(0,0);
+    }
+}
